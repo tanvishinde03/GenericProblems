d1812902672f2a5b38b68dbeaf53163e70523739
tanvishinde03 <shindetanvi0203@gmail.com>
2023-02-22 16:22:26 +0530

    [Tanvi] Add : UC1 - Finding Maximum Int Value

---
+++ GenericProblems/FindMaxValGenerics.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericProblems
+{
+        public class FindMaxGenric<T> where T : IComparable
+        {
+            public T GenricValue(T firstNum, T secondNum, T thirdNum)
+            {
+                if (firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) > 0)
+                    return firstNum;
+                else if (secondNum.CompareTo(thirdNum) > 0 && secondNum.CompareTo(firstNum) > 0)
+                    return secondNum;
+                else
+                    return thirdNum;
+                Console.ReadLine();
+            }
+
+
+        }
+    }
---
+++ GenericProblems/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericProblems
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Choose Number");
+            Console.WriteLine("Enter 1:Finding Maximum Int Numbers ");
+
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    FindMaxVal.FindIntMaxVal(3, 5, 1);
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
