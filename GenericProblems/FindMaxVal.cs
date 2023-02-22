58477b093257f6364bb1ece584d629a55db2d2cc
tanvishinde03 <shindetanvi0203@gmail.com>
2023-02-22 17:36:10 +0530

    [Tanvi] Add : Refactor All The 3 To One Generic Method And Find Max

--- GenericProblems/FindMaxVal.cs
+++ GenericProblems/FindMaxVal.cs
@@ -64,6 +64,19 @@ namespace GenericProblems
                 Console.ReadLine();
             }
         }
+        public T GenricValue<T>(T FirstValue, T SecondValue, T ThirdValue) where T : IComparable
+        {
+            if (FirstValue.CompareTo(SecondValue) > 0 && FirstValue.CompareTo(ThirdValue) > 0)
+                return FirstValue;
+            if (SecondValue.CompareTo(ThirdValue) > 0 && SecondValue.CompareTo(FirstValue) > 0)
+                return SecondValue;
+            if (ThirdValue.CompareTo(FirstValue) > 0 && ThirdValue.CompareTo(SecondValue) > 0)
+                return ThirdValue;
+
+            return FirstValue;
+        }
     }
-    }
+}
+    
+   
 
